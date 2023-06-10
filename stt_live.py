61057faab28d95102e53adab367ddb35b0c44b02
Jason Yosinski <jason@yosinski.com>
2023-06-10 12:43:23 -0700

    Better power meter, save all audio, allow loading audio

--- stt_live.py
+++ stt_live.py
@@ -9,6 +9,7 @@ import whisper
 import pyaudio
 import wave
 import numpy as np
+from datetime import datetime
 
 
 whisper_model_choices = ['tiny', 'base', 'small', 'medium', 'large']
@@ -18,11 +19,13 @@ FORMAT = pyaudio.paInt16
 NPFORMAT = np.int16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 3
+DEFAULT_RECORD_SECONDS = 3
 #WAVE_OUTPUT_FILENAME = 'frompy.wav'
 
 # Max readings**2 for a chunck of length 2048
-heuristic_max_power = 11846935
+#heuristic_max_power = 11846935
+# Float
+heuristic_max_power = 826346439192.0
 
 
 def main():
@@ -31,52 +34,66 @@ def main():
                                      )
     parser.add_argument('--model', '-m', type=str, default='base', choices=whisper_model_choices,
                         help='Which whisper model to use.')
-    parser.add_argument('--saveto', '-o', type=str, default='frompy.wav', help='Filename to write audio to.')
+    parser.add_argument('--saveto', '-o', type=str, default='frompy', help='Filename prefix to record to.')
+    parser.add_argument('--load_audio', '-l', type=str, default=None, help='Skip recording and use audio loaded from LOAD_AUDIO instead.')
+    parser.add_argument('--seconds', '-s', type=float, default=DEFAULT_RECORD_SECONDS, help='How many seconds to record.')
     parser.add_argument('--verbose', '-v', action='store_true', help='Display more information.')
     parser.add_argument('--embed', '-e', action='store_true', help='Embed in IPython at end.')
     args = parser.parse_args()
 
-    # https://stackoverflow.com/questions/40704026/voice-recording-using-pyaudio
-    pp = pyaudio.PyAudio()
-    stream = pp.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK)
+    now = datetime.now()
+    datestamp = now.strftime('%y%m%d_%H%M%S')
 
-    print("*** recording ***")
-    frames = []
+    saveto = f'{args.saveto}_{datestamp}.flac'
 
-    max_power = 0.0
-    for ii in range(int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-        npdat = np.frombuffer(data, dtype=np.int16)
-        power = (npdat**2).sum()
-        frac = min(1.0, power / heuristic_max_power)
-        st = '*' * int(frac**2 * 40)
-        #print(f'{ii}: {npdat.sum()}   {sum(data)} {power}')
-        #print(f'{ii:02d}: {st}')
-        if ii % 2 == 0:
-            print(f'{st}')
-        max_power = max(power, max_power)
+    if args.load_audio is not None:
+        audio_filename = args.load_audio
+    else:
+        # https://stackoverflow.com/questions/40704026/voice-recording-using-pyaudio
+        pp = pyaudio.PyAudio()
+        stream = pp.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK)
 
-    stream.stop_stream()
-    stream.close()
-    pp.terminate()
+        print("*** recording ***")
+        frames = []
 
-    wf = wave.open(args.saveto, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(pp.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+        max_power = 0.0
+        for ii in range(int(RATE / CHUNK * args.seconds)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+            npdat = np.frombuffer(data, dtype=np.int16).astype(np.float64)
+            power = (npdat**2).sum()
+            frac = min(1.0, power / heuristic_max_power)
+            st = '*' * (1 + int(frac ** .125 * 60))
+            #print(f'{ii}: {npdat.sum()}   {sum(data)} {power}')
+            #print(f'{ii:02d}: {st}')
+            if ii % 2 == 0:
+                #print((npdat**2)[:12])
+                print(f'{frac:.04f} {st}')
+            max_power = max(power, max_power)
 
-    print(f'\nWrote file: {args.saveto}')
+        print('Max power was: ', max_power)
+        stream.stop_stream()
+        stream.close()
+        pp.terminate()
+
+        wf = wave.open(saveto, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(pp.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+        print(f'\nWrote file: {saveto}')
+
+        audio_filename = saveto
 
     model = whisper.load_model(args.model)
-    result = model.transcribe(args.saveto)
+    result = model.transcribe(audio_filename)
     text = result['text']
     print(f'\nHere is what I heard:\n{text}')
     
