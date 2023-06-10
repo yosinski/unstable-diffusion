a0e52e45dfa2d8f13313e72183243f13803ad0c5
Jason Yosinski <jason@yosinski.com>
2023-06-10 12:54:47 -0700

    Pulled make_parser into separate file, make stt_model script

---
+++ make_parser.py
@@ -0,0 +1,60 @@
+import argparse
+
+DEFAULT_RECORD_SECONDS = 3
+
+whisper_model_choices = ['tiny', 'base', 'small', 'medium', 'large']
+
+
+def make_parser(descrip=''):
+    parser = argparse.ArgumentParser(description=descrip,
+                                     formatter_class=lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog)
+                                     )
+
+
+    # Args from call_mode
+    parser.add_argument(
+        '--model', type=str, default='text-ada-001', help='openai model name')
+    parser.add_argument(
+        '--prompt_name',
+        type=str,
+        default='test_prompt',
+        help='path to the prompt file')
+    parser.add_argument(
+        '--test_input_name',
+        type=str,
+        default='test',
+        help='path to the user input file')
+    parser.add_argument(
+        '--split_start',
+        type=str,
+        default='Answer: ',
+        help='token indicating start of answer')
+    parser.add_argument(
+        '--split_end',
+        type=str,
+        default=None,
+        help='token indicating end of answer')
+    parser.add_argument(
+        '--temperature', type=float, default=0.5, help='temperature')
+    parser.add_argument(
+        '--max_decoding_steps', type=int, default=128, help='max tokens')
+    parser.add_argument(
+        '--stop_token',
+        type=str,
+        default='',
+        help='stop decoding on token')
+    parser.add_argument(
+        '--api_key', type=str, default=None, help='openai api key to use')
+
+
+    # Args from stt
+    parser.add_argument('--whisper-model', '-w', type=str, default='base', choices=whisper_model_choices,
+                        help='Which Whisper model to use.')
+    parser.add_argument('--saveto', '-o', type=str, default='frompy', help='Filename prefix to record to.')
+    parser.add_argument('--load-audio', '-l', type=str, default=None, help='Skip recording and use audio loaded from LOAD_AUDIO instead.')
+    parser.add_argument('--seconds', '-s', type=float, default=DEFAULT_RECORD_SECONDS, help='How many seconds to record.')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Display more information.')
+    parser.add_argument('--embed', '-e', action='store_true', help='Embed in IPython at end.')
+
+
+    return parser
---
+++ stt_model_loop.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import argparse
+from IPython import embed
+import ipdb as pdb
+import whisper
+import pyaudio
+import wave
+import numpy as np
+from datetime import datetime
+
+from make_parser import make_parser
+
+
+CHUNK = 1024 * 2
+FORMAT = pyaudio.paInt16
+NPFORMAT = np.int16
+CHANNELS = 1
+RATE = 44100
+#WAVE_OUTPUT_FILENAME = 'frompy.wav'
+
+# Max readings**2 for a chunck of length 2048
+#heuristic_max_power = 11846935
+# Float
+heuristic_max_power = 826346439192.0
+
+
+def main():
+    parser = make_parser('Record some audio and convert it to text using the whisper API.')
+    args = parser.parse_args()
+
+    now = datetime.now()
+    datestamp = now.strftime('%y%m%d_%H%M%S')
+
+    saveto = f'{args.saveto}_{datestamp}.flac'
+
+    if args.load_audio is not None:
+        audio_filename = args.load_audio
+    else:
+        # https://stackoverflow.com/questions/40704026/voice-recording-using-pyaudio
+        pp = pyaudio.PyAudio()
+        stream = pp.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK)
+
+        print("*** recording ***")
+        frames = []
+
+        max_power = 0.0
+        for ii in range(int(RATE / CHUNK * args.seconds)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+            npdat = np.frombuffer(data, dtype=np.int16).astype(np.float64)
+            power = (npdat**2).sum()
+            frac = min(1.0, power / heuristic_max_power)
+            st = '*' * (1 + int(frac ** .125 * 60))
+            #print(f'{ii}: {npdat.sum()}   {sum(data)} {power}')
+            #print(f'{ii:02d}: {st}')
+            if ii % 1 == 0:
+                #print((npdat**2)[:12])
+                print(f'{frac:.04f} {st}')
+            max_power = max(power, max_power)
+
+        print('Max power was: ', max_power)
+        stream.stop_stream()
+        stream.close()
+        pp.terminate()
+
+        wf = wave.open(saveto, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(pp.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+        print(f'\nWrote file: {saveto}')
+
+        audio_filename = saveto
+
+    model = whisper.load_model(args.whisper_model)
+    result = model.transcribe(audio_filename)
+    text = result['text']
+    print(f'\nHere is what I heard:\n{text}')
+    
+    if args.embed:
+        embed()
+
+
+if __name__ == '__main__':
+    main()
